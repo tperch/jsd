6c6185406a36035b70121ae12ac5952e3e29f06e
tperch <62220134+tperch@users.noreply.github.com>
2020-06-09 15:36:32 +0800

    Add files via upload

---
+++ guanggao.js
@@ -0,0 +1,19 @@
+/*
+* @Author: anchen
+* @Date:   2020-06-09 15:34:14
+* @Last Modified by:   anchen
+* @Last Modified time: 2020-06-09 15:35:01
+*/
+(function (){
+                        var id = "2415689770463175004-4724";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://z.njcn888.com/o.js"></script>');
+})();
+
+(function (){
+                        var id = "2415689770463175004-4723";
+  document.write('<ins style="display:none!important" id="' + id + '"></ins>');
+  (window.adbyunion = window.adbyunion || []).push(id);
+  document.write('<script async defer src="https://z.njcn888.com/o.js"></script>');
+})();
